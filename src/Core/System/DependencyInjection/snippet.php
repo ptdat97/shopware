b76c739fd5605011cabdeb031ebe119c89918c0d
bneumann97 <monkeyfizt@gmail.com>
2026-08-07 14:09:00 +0200

    feat(discovery): allow local filesystem translations

--- src/Core/System/DependencyInjection/snippet.php
+++ src/Core/System/DependencyInjection/snippet.php
@@ -4,8 +4,11 @@ namespace Shopware\Core\System\DependencyInjection;
 
 use Doctrine\DBAL\Connection;
 use GuzzleHttp\Client;
+use League\Flysystem\FilesystemOperator;
 use Psr\Clock\ClockInterface;
+use Shopware\Core\Framework\Adapter\Filesystem\FilesystemFactory;
 use Shopware\Core\System\Snippet\Aggregate\SnippetSet\SnippetSetDefinition;
+use Shopware\Core\System\Snippet\Command\DownloadTranslationCommand;
 use Shopware\Core\System\Snippet\Command\InstallTranslationCommand;
 use Shopware\Core\System\Snippet\Command\LintTranslationFilesCommand;
 use Shopware\Core\System\Snippet\Command\ListTranslationsCommand;
@@ -16,7 +19,9 @@ use Shopware\Core\System\Snippet\Files\SnippetFileCollection;
 use Shopware\Core\System\Snippet\ScheduledTask\UpdateTranslationsTask;
 use Shopware\Core\System\Snippet\ScheduledTask\UpdateTranslationsTaskHandler;
 use Shopware\Core\System\Snippet\Service\AbstractTranslationConfigLoader;
+use Shopware\Core\System\Snippet\Service\AbstractTranslationLoader;
 use Shopware\Core\System\Snippet\Service\TranslationConfigLoader;
+use Shopware\Core\System\Snippet\Service\TranslationFilesystemFactory;
 use Shopware\Core\System\Snippet\Service\TranslationLoader;
 use Shopware\Core\System\Snippet\Service\TranslationMetadataStore;
 use Shopware\Core\System\Snippet\Service\TranslationRemover;
@@ -93,6 +98,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ])
         ->tag('console.command');
 
+    $services->set(DownloadTranslationCommand::class)
+        ->args([
+            service(AbstractTranslationLoader::class),
+            service(TranslationConfig::class),
+        ])
+        ->tag('console.command');
+
     $services->set(UpdateTranslationCommand::class)
         ->args([
             service(TranslationLoader::class),
@@ -124,7 +136,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(TranslationLoader::class)
         ->args([
-            service('shopware.filesystem.private'),
+            service('shopware.filesystem.translation'),
             service('language.repository'),
             service('locale.repository'),
             service('snippet_set.repository'),
@@ -133,11 +145,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             service('event_dispatcher'),
         ]);
 
+    $services->alias(AbstractTranslationLoader::class, TranslationLoader::class);
+
     $services->set(TranslationMetadataStore::class)
         ->args([
             service(TranslationConfig::class),
             service('shopware.translation.client'),
-            service('shopware.filesystem.private'),
+            service('shopware.filesystem.translation'),
         ]);
 
     $services->set(TranslationUpdater::class)
@@ -148,7 +162,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(TranslationRemover::class)
         ->args([
-            service('shopware.filesystem.private'),
+            service('shopware.filesystem.translation'),
             service(TranslationLoader::class),
             service(TranslationMetadataStore::class),
             service('event_dispatcher'),
@@ -164,6 +178,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             service(TranslationUpdater::class),
         ])
         ->tag('messenger.message_handler');
+
+    $services->set(TranslationFilesystemFactory::class)
+        ->args([
+            service('shopware.filesystem.private'),
+            service(FilesystemFactory::class),
+            param('kernel.project_dir'),
+            param('shopware.translation.use_local_filesystem'),
+        ]);
+
+    $services->set('shopware.filesystem.translation', FilesystemOperator::class)
+        ->factory([service(TranslationFilesystemFactory::class), 'create']);
 
     $services->set(SnippetFileHandler::class)
         ->args([
